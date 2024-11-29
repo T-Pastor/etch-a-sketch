4f4b62f2b624f634e07abe68190869f1dbcee2e3
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-28 20:51:39 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.getElementById("container");
+
+
+function makeGrid(rows, columns) {
+    
+    for(let i = 0; i < (rows * columns); i++) {
+        let box = document.createElement("div");
+        container.appendChild(box).className = "item";
+    }
+}
+
+makeGrid(16, 16);
