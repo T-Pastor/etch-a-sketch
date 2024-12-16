cd271000deeb70c1c8728eb7b5f60092b6cfad03
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-12-15 20:42:54 -0500

    pixels change color when moused over, users can specify columns and rows

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
 const container = document.getElementById("container");
+const button = document.getElementById("reset");
+const screenWidth = document.documentElement.clientWidth;
 
 
 function makeGrid(rows, columns) {
-    
-    for(let i = 0; i < (rows * columns); i++) {
-        let box = document.createElement("div");
-        container.appendChild(box).className = "item";
+    const boxSize = screenWidth / columns;
+    container.style.width = screenWidth + "px";
+    container.style.height = boxSize + "px";
+
+    for(let i = 0; i < rows; i++) {
+        let row = document.createElement("div");
+
+        for(let j = 0; j < columns; j++) {
+            const pixel = document.createElement("div");
+            pixel.id = "pixel";
+            pixel.style.width = boxSize + "px";
+            pixel.style.height = boxSize + "px";
+            row.appendChild(pixel);
+        }
+        container.appendChild(row);
     }
 }
 
-makeGrid(16, 16);
+makeGrid(16, 16)
 
 container.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "purple";
 })
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+button.addEventListener("click", () => {
+
+    let rows = getInput(0);
+    let columns = getInput(1);
+
+    if (rows > 0 && columns > 0) {
+        clearGrid();
+        makeGrid(rows, columns);
+    }
+}) 
+
+function getInput(inputType) {
+    let input = 0;
+    let correctInput = false;
+    
+    while (!correctInput) {
+        if (inputType == 0) {
+            input = prompt("How many rows?");
+        } else if (inputType == 1) {
+            input = prompt("How many columns?");
+        }
+        correctInput = validateEntry(input);
+    }
+    return input;       
+}
+
+function validateEntry (input) {
+    if (!input) { //if cancel button is pressed
+        return -1;
+    } else if (input > 100 || input <= 0 || isNaN(input)) { //verify input is a number between 1 and 100
+        alert("Invalid entry. Please enter a number between 1 and 100.");
+        return false;
+    } else {
+        return true;
+    }
+}
